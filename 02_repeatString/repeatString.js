7ceee23cd2d042657862d7bc79f687e291ea50fc
Nicholas Nguyen <nicholas.p.nguyen@gmail.com>
2024-01-03 18:56:35 -0500

    Solution for JS assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    let i;
+    let error = "ERROR"
+    if (num < 0) 
+    {
+        return error;
+    }
+    
+    for (i = 0; i < num; i++) 
+    {
+        result += string; 
+    }
 
+    return result;
 };
 
 // Do not edit below this line

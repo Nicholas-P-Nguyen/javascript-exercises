d839d59c06e86d96c82cf9fede40df5d461ece87
Nicholas Nguyen <nicholas.p.nguyen@gmail.com>
2024-01-03 23:29:20 -0500

    Solutions for assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let i;
+    let numBig;
+    let numSmall;
+    let output = 0; 
+    
+    // Edge case
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) 
+    {
+        return "ERROR";
+    }    
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
 
+    // Determine the bigger number
+    if (numOne > numTwo)
+    {
+        numBig = numOne;
+        numSmall = numTwo;
+    }
+    else
+    {
+        numBig = numTwo;
+        numSmall = numOne;
+    }
+
+    for (i = numSmall; i <= numBig; i++) 
+    {
+        output += i;
+    }
+    return output;
+    
 };
 
 // Do not edit below this line

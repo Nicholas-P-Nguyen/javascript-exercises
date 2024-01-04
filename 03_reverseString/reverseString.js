36f40ad359789dafd4abd86276cafb77b3bb648b
Nicholas Nguyen <nicholas.p.nguyen@gmail.com>
2024-01-03 19:43:52 -0500

    solution for assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let charArray = text.split('');
+    let lengthOfText = text.length - 1;
+    let i;
+    let result = "";
+    for (i = lengthOfText; i >= 0; i--) 
+    {
+        result += charArray[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
